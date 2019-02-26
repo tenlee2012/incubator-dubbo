eb3b68fa487d913afb97b431023706256b671c1c
lijiahao <lijiahao@fangdd.com>
2019-02-26 15:04:18 +0800

    use zookeeper

--- dubbo-demo/dubbo-demo-annotation/dubbo-demo-annotation-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
+++ dubbo-demo/dubbo-demo-annotation/dubbo-demo-annotation-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
@@ -36,7 +36,7 @@ public class Application {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConsumerConfiguration.class);
         context.start();
         DemoService service = context.getBean("demoServiceComponent", DemoServiceComponent.class);
-        String hello = service.sayHello("world");
+        String hello = service.sayHello("world222");
         System.out.println("result :" + hello);
     }
 
